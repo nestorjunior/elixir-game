60ef23958f89fcdece75dd697491a4c12353f191
Nestor Júnior <contato.nestor@gmail.com>
2025-06-25 11:36:41 -0300

    refactor: module player

--- lib/player.ex
+++ lib/player.ex
@@ -1,7 +1,9 @@
 defmodule Player do
-  @enforce_keys [:name, :life, :move_rnd, :move_avg, :move_heal]
-  
-  defstruct [:name, :life, :move_rnd, :move_avg, :move_heal]
+
+  require_keys = [:name, :life, :move_rnd, :move_avg, :move_heal]
+
+  @enforce_keys require_keys
+  defstruct require_keys
 
   def build(name, life, move_rnd, move_avg, move_heal) do
     %Player{name: name, life: life, move_rnd: move_rnd, move_avg: move_avg, move_heal: move_heal}
